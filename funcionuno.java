8d9429c832864c88cc3250ef2e42176426ca0073
Sarayval <1001117794@teinco.edu.co>
2025-03-26 10:40:02 -0500

    funcionuno.java

--- funcionuno.java
+++ funcionuno.java
@@ -1,16 +1,13 @@
-// Creación de la función sumar dos numeros en Java
-Public class Ejemplo {
+public class Ejemplo {
 
     // Creación de la función
-
-    Public static int sumar (int a , int b) {
-        return a + b ;
+    public static int sumar(int a, int b) {
+        return a + b;
     }
 
-    // Ejecución de la función sumar 
-
-    Public static void main(String[] args) {
-        int resultado = sumar (2 , 10);
-        System.out.println (resultado);
+    // Ejecución de la función sumar
+    public static void main(String[] args) {
+        int resultado = sumar(2, 10);
+        System.out.println(resultado);
     }
-} 
+}
