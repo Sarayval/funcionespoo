ff56ad3a1d7be46f0797416479771a7cb2c241d9
Sarayval <1001117794@teinco.edu.co>
2025-03-26 10:34:38 -0500

    Add files via upload

---
+++ funcionuno.java
@@ -0,0 +1,16 @@
+// Creación de la función sumar dos numeros en Java
+Public class Ejemplo {
+
+    // Creación de la función
+
+    Public static int sumar (int a , int b) {
+        return a + b ;
+    }
+
+    // Ejecución de la función sumar 
+
+    Public static void main(String[] args) {
+        int resultado = sumar (2 , 10);
+        System.out.println (resultado);
+    }
+} 
