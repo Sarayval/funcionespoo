6c26713c408e306c0cda77229f56b30931999711
Sarayval <1001117794@teinco.edu.co>
2025-03-26 10:46:45 -0500

    funciondos.py

--- funciondos.py
+++ funciondos.py
@@ -1,19 +1,18 @@
 import random
 
 # Realizar función probabilidad
+def probabilidad(num_lanzamientos, num_caras):
 
-def probabilidad(num_lanzamientos,num_caras):
+    # Simular los lanzamientos 
+    resultados = [random.choice([0, 1]) for _ in range(num_lanzamientos)]
+    
+    # Calcular la probabilidad de obtener el número de caras (1)
+    prob = resultados.count(1) / num_lanzamientos
+    return prob
 
-#Simular los lanzamientos 
-resultados=[random.choice([0,1])] for _ in range(num_lanzamientos)
+# Ejemplos de uso 
+num_lanzamientos = 10
+num_caras = 6  
 
-#Calcular la probabilidad de obtener el número caras
-prob=resultados.count(1)/num_lanzamientos
-return prob
-
-#Ejemplos de uso (Declaración de objetos)
-num_lanzamientos=10
-num_caras=6
-
-prob=probabilidad(num_lanzamientos,num_caras)
-print (f "la probabilidad de obtener {num_lanzamientos} es {prob:4f}")
+prob = probabilidad(num_lanzamientos, num_caras)
+print(f"La probabilidad de obtener caras en {num_lanzamientos} lanzamientos es {prob:.4f}")
